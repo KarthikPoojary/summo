21aed56401329ce0417ff513e5f6d425d136cd5b
KarthikPoojary <kpoojary20@gmail.com>
2018-10-12 20:24:27 +0530

    Changed contact form mail subject

--- send_form_email.php
+++ send_form_email.php
@@ -4,7 +4,7 @@ if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "enquiry@summobrand.com";
-    $email_subject = "Your email subject line";
+    $email_subject = "Contact Us Form || Summo Brandcom Website";
  
     function died($error) {
         // your error code can go here
